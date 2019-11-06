8d8fbcc8771e38975f25332ce8a400ed2e460431
Jorrit Salverda <jsalverda@travix.com>
2019-11-06 16:17:43 +0100

    use personal access token via token parameter, since server to server tokens are disallowed

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"os"
 	"runtime"
@@ -19,7 +18,7 @@ var (
 
 var (
 	// flags
-	apiTokenJSON = kingpin.Flag("credentials", "Github api token credentials configured at the CI server, passed in to this trusted extension.").Envar("ESTAFETTE_CREDENTIALS_GITHUB_API_TOKEN").Required().String()
+	accessToken  = kingpin.Flag("token", "Github personal access token.").Envar("ESTAFETTE_EXTENSION_TOKEN").Required().String()
 	gitRepoOwner = kingpin.Flag("git-repo-owner", "The owner of the Github repository.").Envar("ESTAFETTE_GIT_OWNER").Required().String()
 	gitRepoName  = kingpin.Flag("git-repo-name", "The name of the Github repository.").Envar("ESTAFETTE_GIT_NAME").Required().String()
 )
@@ -36,21 +35,11 @@ func main() {
 	// log startup message
 	log.Printf("Starting estafette-extension-github-pages version %v...", version)
 
-	// get api token from injected credentials
-	var credentials []APITokenCredentials
-	err := json.Unmarshal([]byte(*apiTokenJSON), &credentials)
-	if err != nil {
-		log.Fatal("Failed unmarshalling injected credentials: ", err)
-	}
-	if len(credentials) == 0 {
-		log.Fatal("No credentials have been injected")
-	}
-
 	// set build status
-	githubAPIClient := newGithubAPIClient(credentials[0].AdditionalProperties.Token)
+	githubAPIClient := newGithubAPIClient(*accessToken)
 
 	// get milestone by version
-	err = githubAPIClient.RequestPageBuild(*gitRepoOwner, *gitRepoName)
+	err := githubAPIClient.RequestPageBuild(*gitRepoOwner, *gitRepoName)
 	if err != nil {
 		log.Fatalf("Requesting page build failed. %v", err)
 	}
